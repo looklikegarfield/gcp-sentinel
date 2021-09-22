691191c4aa455fa9340d0635a1a4bf2e3d4e56d1
abhishektiwari8 <65304638+abhishektiwari8@users.noreply.github.com>
2021-09-22 13:46:04 +0530

    updated

---
+++ variable.tf
@@ -0,0 +1,41 @@
+
+#
+#VPC Peering name in Cloud Exchange VPC
+#
+VPCPeeringProName = "gisshareddev001"
+#Peering_Shared="shared"
+Peering_CloudExchange="cloudexchange"
+
+
+variable "NetworkName" {
+  type        = string
+  default     = "vpc-shared-dev-01"
+  description = "The network routing mode (default 'GLOBAL')"
+}
+
+variable "SubnetName_usw2" {
+  type        = string
+  default     = "sub-prv-usw2-01"
+  description = "The network routing mode (default 'GLOBAL')"
+}
+
+variable "SubnetRegion_usw2" {
+  type        = string
+  default     = "us-west2"
+  description = "The network routing mode (default 'GLOBAL')"
+}
+
+variable "IP_CIDR_Range_subnet_usw2" {
+  type        = string
+  default     = "10.10.8.0/24"
+  description = "The network routing mode (default 'GLOBAL')"
+}
+
+variable "access_key" {
+  type        = string
+  description = "The network routing mode (default 'GLOBAL')"
+}
+
+variable "project_id" {
+  type        = string
+}
