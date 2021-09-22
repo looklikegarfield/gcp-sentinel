fcf5303095bbcca8ef02e54591e43c957ff6aa3d
abhishektiwari8 <65304638+abhishektiwari8@users.noreply.github.com>
2021-09-22 13:03:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,4 @@
-/**
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+variable "access_key" {}
 
 variable "project_id" {
   description = "The ID of the project where this VPC will be created"
@@ -20,6 +6,7 @@ variable "project_id" {
 
 variable "network_name" {
   description = "The name of the network being created"
+  default     = "shared_network"
 }
 
 variable "routing_mode" {
@@ -67,7 +54,7 @@ variable "delete_default_internet_gateway_routes" {
 variable "description" {
   type        = string
   description = "An optional description of this resource. The resource must be recreated to modify this field."
-  default     = ""
+  default     = "test"
 }
 
 variable "auto_create_subnetworks" {
