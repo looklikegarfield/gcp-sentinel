570955286a780759388b6fc70e322446887c640f
abhishektiwari8 <65304638+abhishektiwari8@users.noreply.github.com>
2021-09-22 14:48:51 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,30 +3,26 @@
 variable "NetworkName" {
   type        = string
   default     = "vpc-shared-dev-01"
-  description = "The network routing mode (default 'GLOBAL')"
 }
 
 variable "SubnetName_usw2" {
   type        = string
   default     = "sub-prv-usw2-01"
-  description = "The network routing mode (default 'GLOBAL')"
 }
 
 variable "SubnetRegion_usw2" {
   type        = string
   default     = "us-west2"
-  description = "The network routing mode (default 'GLOBAL')"
 }
 
 variable "IP_CIDR_Range_subnet_usw2" {
   type        = string
   default     = "10.10.8.0/24"
-  description = "The network routing mode (default 'GLOBAL')"
 }
 
 variable "access_token" {
   type        = string
-  description = "The network routing mode (default 'GLOBAL')"
+  sensitive   = true
 }
 
 variable "project_id" {
